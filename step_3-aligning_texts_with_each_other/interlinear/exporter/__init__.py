6d20bddaed9856c9e8c078110a914a96ada0269f
p-freeman <70345349+p-freeman@users.noreply.github.com>
2026-02-10 02:41:06 +0100

    Update __init__.py

--- step_3-aligning_texts_with_each_other/interlinear/exporter/__init__.py
+++ step_3-aligning_texts_with_each_other/interlinear/exporter/__init__.py
@@ -1,1 +1,8 @@
-# exporter package
+# Exporter subpackage
+from .html_export import (
+    generate_basic,
+    generate_with_audio,
+    generate_with_youtube,
+    generate_mobile_app_html,
+    generate_app_package
+)
